c73e7a16553a48f11b738eb6370f20a4c7074e69
maartynaa <martyna.kr.maciejewska@student.put.poznan.pl>
2018-12-06 11:12:46 +0100

    obsługa radio buttonów

--- gry/src/main/java/com/sample/DroolsTest.java
+++ gry/src/main/java/com/sample/DroolsTest.java
@@ -1,6 +1,8 @@
 package com.sample;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
 import org.kie.api.KieServices;
@@ -20,9 +22,9 @@ public class DroolsTest {
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-        	Frame frame = new Frame();
+        	Frame frame = new Frame(null);
         	kSession.insert(frame);
-            // go !
+        	
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
@@ -55,6 +57,26 @@ public class DroolsTest {
         public void setNazwa(String nazwa) {
             this.nazwa = nazwa;
         }
+
+    }
+    
+    public static class Message {
+
+        private String mes;
+        
+        
+        public Message(String mes){
+        	this.mes = mes;
+        }
+
+        public String getMes() {
+            return this.mes;
+        }
+
+        public void setMes(String mes) {
+            this.mes = mes;
+        }
+
 
     }
 
--- gry/src/main/java/com/sample/Frame.java
+++ gry/src/main/java/com/sample/Frame.java
@@ -1,32 +1,69 @@
 package com.sample;
 
-import java.awt.*;
+import java.awt.EventQueue;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 
 public class Frame extends JFrame {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	Frame(){
-	    	gui();
-	}
+	/**
+	 * Create the frame.
+	 */
 	
-	public void gui() {
+	JButton btnOk;
+	private String odp;
+	boolean pom = true;
+	
+	
+	public Frame(String odp) {
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setVisible(true);
-    	setSize(300, 300);
-    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	
-   
-    	JButton b = new JButton("OK");
-    	add(b, BorderLayout.SOUTH);
-    	
-    	
-    	
+		btnOk = new JButton("OK");
+		
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		btnOk.setFont(new Font("Tahoma", Font.BOLD, 16));
+		getContentPane().add(btnOk, BorderLayout.SOUTH);
+		
+		this.odp = odp;
+
 	}
 	
+	public void Action3(
+			JRadioButton r1, JRadioButton r2,JRadioButton r3, String a, String b, String c, ButtonGroup g) {
+			
+		while(pom == true) {
+		
+		 btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				r1.setActionCommand(a);
+				r2.setActionCommand(b);
+				r3.setActionCommand(c);
+				String rev = g.getSelection().getActionCommand();	
+				setOdp(rev);
+				pom = false;
+			}
+		});
+		 
+		}
+		
+	}
+
 	
+	public String getOdp() {
+        return this.odp;
+    }
+
+    public void setOdp(String odp) {
+        this.odp = odp;
+    }
+
 }
