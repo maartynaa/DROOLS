74e6e33e974352566fc024ef4abdc25ee560a67d
maartynaa <44441407+maartynaa@users.noreply.github.com>
2018-12-07 06:47:50 +0100

    main

--- gry/src/main/java/com/sample/DroolsTest.java
+++ gry/src/main/java/com/sample/DroolsTest.java
@@ -1,11 +1,7 @@
 package com.sample;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-
-import javax.swing.*;
-
 import org.kie.api.KieServices;
+import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
@@ -15,71 +11,20 @@ import org.kie.api.runtime.KieSession;
 public class DroolsTest {
 
     public static final void main(String[] args) {
-    	
-    	
         try {
             // load up the knowledge base
-	        KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
-        	Frame frame = new Frame();
-        	Message message = new Message(null);
-        	kSession.insert(frame);
-        	kSession.insert(message);
-        	
+        	KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
+            KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
+
+            // go !
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
+
     
-    
-    public static class Gra {
-
-        private String typ;
-        private String nazwa;
-        
-        public Gra(String nazwa, String typ){
-        	this.typ = typ;
-        	this.nazwa = nazwa;
-        }
-
-        public String getTyp() {
-            return this.typ;
-        }
-
-        public void setTyp(String typ) {
-            this.typ = typ;
-        }
-
-        public String getNazwa() {
-            return this.nazwa;
-        }
-
-        public void setNazwa(String nazwa) {
-            this.nazwa = nazwa;
-        }
-
-    }
-    
-    public static class Message {
-
-        private String mes;
-        
-        
-        public Message(String mes){
-        	this.mes = mes;
-        }
-
-        public String getMes() {
-            return this.mes;
-        }
-
-        public void setMes(String mes) {
-            this.mes = mes;
-        }
-
-
-    }
 
 }
