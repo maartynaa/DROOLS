7350a4619825d12674dbe89668282cbe8c9464a3
Michał Bień <michal1.bien@gmail.com>
2018-12-07 19:38:52 +0100

    Partial wargame

--- gry/src/main/java/com/sample/DroolsTest.java
+++ gry/src/main/java/com/sample/DroolsTest.java
@@ -1,7 +1,6 @@
 package com.sample;
 
 import org.kie.api.KieServices;
-import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
@@ -16,7 +15,6 @@ public class DroolsTest {
         	KieServices ks = KieServices.Factory.get();
             KieContainer kContainer = ks.getKieClasspathContainer();
             KieSession kSession = kContainer.newKieSession("ksession-rules");
-            KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
 
             // go !
             kSession.fireAllRules();
